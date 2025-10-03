a2e8f5954fa0649b4b1575bae44173ef78a35331
IK <rbwcska@gmail.com.com>
2025-10-03 11:22:06 +0200

    first release

---
+++ 100_days/001_Day/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in? \n").capitalize()
+pet = input("What's your pet's name? \n").capitalize()
+print(f"Your band name could be {city} {pet}")
